bdab877165a0e7ad04b407abb814e5f086ba72fc
kolown2007 <kolown@gmail.com>
2025-07-24 21:32:06 +0800

    Remove secrets.h file containing sensitive information

--- src/config/secrets.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define WS_URL "ws://192.168.1.100:8080/path"
-#define WIFI_ssid "flo"
-#define WIFI_password "birdyboo2019"
-
-// WiFi Configuration
-#define WIFI_SSID_NAME "GhostWhisper"
-#define OFFLINE_AP_PASSWORD "qcb2025"
